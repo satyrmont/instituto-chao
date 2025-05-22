24401f5ddff494be4304c82955bc4d719b23b7dc
satyrmont <bhcp3316@gmail.com>
2025-05-21 22:12:18 -0300

    added supplier map

---
+++ src/pages/4Mapa.page.jsx
@@ -0,0 +1,13 @@
+function Mapa() {
+  return (
+    <div>
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=15LextW38AfDS_751br-7emSPmktAtsI&ehbc=2E312F"
+        width="100%"
+        height="480"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Mapa;
